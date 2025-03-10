77f9bb8e9c06fbed7ec8493bff088db8ad61446c
Suveg <suveg.nimje0@gmail.com>
2025-03-10 11:57:19 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wastedetection',
+    version = '0.0.0',
+    author = 'Suveg',
+    author_email='suveg.nimje0@gmail.com',
+    packages=find_packages(),
+    install_requires= []
+)
